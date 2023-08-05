e980fbe94fb9085318039a1a1bdc8eba241d9f8f
Daniel Masibo <danielmasibo@gmail.com>
2023-08-06 00:36:21 +0300

    initial commit

---
+++ hello.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template
+
+# create a flask instance 
+app = Flask(__name__)
+
+# create a route decorator 
+@app.route("/")
+# def index():
+#     return "<h1>Hello World!!!</h1>"
+
+  # FILTERS
+# safe
+# capitalize
+# upper
+# lower
+# trim
+# title
+# striptags
+
+def index():
+    first_name="John"
+    stuff = "This is bold Text"
+    favorite_pizza = ["Pepperoni", "Cheese", "Mushrooms", 41]
+    return render_template("index.html", 
+                           first_name=first_name, 
+                           stuff=stuff,
+                           favorite_pizza=favorite_pizza)
+
+# localhost:5000/user/John
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template("user.html", user_name=name)
+
+# custom error pages
+
+  # invalid URL 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+  # Internal Server Error 
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+    
+
+
